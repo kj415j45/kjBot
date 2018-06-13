f9025786c4e9c466b11a5e5d7cd1b51524399b84
kj415j45 <kj415j45@gmail.com>
2018-06-13 14:51:01 +0900

    :children_crossing: Make it more powerful

--- module/say/main.php
+++ module/say/main.php
@@ -1,7 +1,24 @@
 <?php
 
-global $Event, $Queue;
-if($Event['user_id']==config('master'))
-$Queue[]= sendBack(nextArg());
+global $Event, $Queue, $Command;
+requireMaster();
+
+do{
+    $arg = nextArg();
+    switch($arg){
+        case '-escape':
+            $escape = true;
+            break;
+        case '-async':
+            $async = true;
+            break;
+        default:
+
+    }
+}while($arg !== NULL);
+
+$Queue[]= sendBack(
+          substr($Event['message'], strpos($Event['message'], "\n")+1)
+          , $escape, $async);
 
 ?>
