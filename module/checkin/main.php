3b66006717ba0eb511a1b8efd1ac90c9deca4451
kj415j45 <kj415j45@gmail.com>
2018-06-13 20:17:29 +0900

    :sparkles: Complete credit and checkin module

---
+++ module/checkin/main.php
@@ -0,0 +1,16 @@
+<?php
+
+global $Event, $Queue;
+loadModule('credit.tools');
+
+$income = rand(10, 25);
+clearstatcache();
+$lastCheckinTime=filemtime('../storage/data/credit/'.$Event['user_id']);
+if(0 == (int)date('d')-(int)date('d', $lastCheckinTime)){
+    $Queue[]= sendBack('你今天签到过了');
+}else{
+    addCredit($Event['user_id'], $income);
+    $Queue[]= sendBack('签到成功，获得 '.$income.' 个金币');
+}
+
+?>
