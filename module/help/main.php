ee5eaecf4ab3bd55bdd3af40cb6de7f386d5acb4
kj415j45 <kj415j45@gmail.com>
2018-06-16 23:08:40 +0900

    :sparkles: Add welcome message

---
+++ module/help/main.php
@@ -0,0 +1,14 @@
+<?php
+
+global $Event;
+$msg=<<<EOT
+可用命令：
+checkin
+credit
+osu
+version
+EOT;
+
+$Queue[]= sendBack($msg);
+
+?>
---
+++ public/eventProcessor.php
@@ -0,0 +1,18 @@
+<?php
+
+global $Event, $Queue;
+use kjBot\SDK\CQCode;
+
+switch($Event['event']){
+    case 'group_increase':
+        if($Event['user_id'] != config('bot')){
+            $Queue[]= sendBack(CQCode::At($Event['user_id']).' 欢迎加入本群，请阅读群公告！');
+        }else{
+            $Queue[]= sendBack('kjBot 已入驻本群，发送 '.config('prefix', '!').'help 查看帮助');
+        }
+        break;
+    default:
+        
+}
+
+?>
