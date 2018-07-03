6c71d4a810135f7d481618e3bd0b2d8330cf94ae
kj415j45 <kj415j45@gmail.com>
2018-07-03 19:22:39 +0900

    :sparkles: Use Github App to create issue by bot account

--- module/issue/main.php
+++ module/issue/main.php
@@ -1,6 +1,12 @@
 <?php
 
 global $Queue, $Text, $Event;
+use Http\Adapter\Guzzle6\Client as GuzzleClient;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+
+$builder = new Github\HttpClient\Builder(new GuzzleClient());
 
 $length = strpos($Text, "\r");
 if(false===$length)$length=strlen($Text);
@@ -9,18 +15,28 @@ $body = substr($Text, $length+2);
 
 if($title == '')leave('请提供 issue 标题');
 
-if(coolDown("issue/{$Event['user_id']}")<0);leave('冷却中');
+if(coolDown("issue/{$Event['user_id']}")<0)leave('冷却中');
 coolDown("issue/{$Event['user_id']}", 60*60*24);
 
-$Github = new \Github\Client();
-$Github->authenticate(config('GITHUB_TOKEN'), '', \Github\Client::AUTH_HTTP_TOKEN);
+$github = new Github\Client($builder, 'machine-man-preview');
+$jwt = (new Builder)
+    ->setIssuer(config('Github_Integration_ID'))
+    ->setIssuedAt(time())
+    ->setExpiration(time() + 60)
+    ->sign(new Sha256(),  new Key(getData('kjBot-Github.pem')))
+    ->getToken();
 
-$result = $Github->api('issue')->create('kj415j45', 'kjBot', [
+$github->authenticate($jwt, null, Github\Client::AUTH_JWT);
+$token = $github->api('apps')->createInstallationToken(config('Github_Installation_ID'));
+$github->authenticate($token['token'], null, Github\Client::AUTH_HTTP_TOKEN);
+
+$result = $github->api('issue')->create('kj415j45', 'kjBot', [
     'title' => '[From Bot] '.$title,
     'body' => '>该 Issue 由 Bot 通过 API 生成，创建者：'.$Event['user_id']."\n\n".$body,
     'assignees' => ['kj415j45'],
 ]);
 
-$Queue[]= sendBack('Issue 创建成功！'.$result['html_url']);  
+$Queue[]= sendBack('Issue 创建成功！'.$result['html_url']);
+$Queue[]= sendMaster($Event['user_id'].' 创建了新 issue '.$result['html_url'])
 
 ?>
