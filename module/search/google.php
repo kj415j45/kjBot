9590079e4f46ad6ad37f336138499425901411e7
kj415j45 <kj415j45@gmail.com>
2018-07-14 13:18:51 +0900

    :sparkles: Add search
    Close #50, need futher improve

---
+++ module/search/google.php
@@ -0,0 +1,11 @@
+<?php
+
+global $Queue;
+$query='';
+do{
+    $arg = nextArg();
+    $query.=urlencode($arg).'+';
+}while($arg!==NULL);
+$Queue[]= sendBack('https://google.com/search?q='.$query);
+
+?>
