9a6dbf28f425aaf943465cff927b14bff9854418
kj415j45 <kj415j45@gmail.com>
2018-07-21 08:50:22 +0900

    :sparkles: Add trans module
    Help message is added

--- module/help/main.php
+++ module/help/main.php
@@ -6,7 +6,7 @@ $msg=<<<EOT
 !help.{命令}
 获得该命令的帮助
 可用命令：
-checkin credit issue osu pixiv recordStat roll search sleep time unsleep version voice
+checkin credit issue osu pixiv recordStat roll search sleep time trans unsleep version voice
 EOT;
 
 $Queue[]= sendBack($msg);
