d78eb0d3053b130fdf763f07adfe159563837deb
kj415j45 <kj415j45@gmail.com>
2018-07-21 09:04:58 +0900

    :memo: Add github link

--- module/version/main.php
+++ module/version/main.php
@@ -4,6 +4,6 @@ global $Queue;
 
 $Github = new \Github\Client();
 $result = $Github->api('repo')->releases()->latest('kj415j45', 'kjBot');
-$Queue[]= sendBack("kjBot {$result['tag_name']} {$result['name']}\n{$result['body']}");
+$Queue[]= sendBack("kjBot {$result['tag_name']} {$result['name']}\n项目地址：https://github.com/kj415j45/kjBot\n{$result['body']}");
 
 ?>
