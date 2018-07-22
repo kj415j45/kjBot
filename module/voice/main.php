239d1e1184d865d6c2ce8ce83dab0ba674969e2a
kj415j45 <kj415j45@gmail.com>
2018-07-22 14:37:50 +0900

    :memo: Better text

--- module/voice/main.php
+++ module/voice/main.php
@@ -4,7 +4,7 @@ global $Event, $Queue, $Text;
 
 $lang = nextArg();
 $hash = $Event['message_id'];
-if(2 !== strlen($lang))leave('未指定语言');
+if(2 !== strlen($lang))leave('语言代号应为两个英文字符');
 
 setCache($hash.'.txt', removeCQCode(removeEmoji($Text)));
 exec("export LC_ALL=C.UTF-8 && export LANG=C.UTF-8 && cd ../storage/cache/ && gtts-cli -f {$hash}.txt -o {$hash}.mp3 -l {$lang}"); //So fucking hardcore py3
