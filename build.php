ac120ca88e71d626e46e016af8fd74584e945259
LovelyA72 <a72@luotianyi.pro>
2018-07-26 13:32:50 -0700

    add windows build support

---
+++ build.php
@@ -0,0 +1,22 @@
+<?php
+file_put_contents("composer-installer", fopen("https://getcomposer.org/installer", 'r'));
+exec("php composer-installer");
+exec("php composer.phar install");
+
+exec("mkdir storage\\");
+exec("mkdir storage\\data\\");
+exec("mkdir storage\\cache\\");
+$db = new SQLite3('storage/data/stat.db');
+
+$sql=<<<EOF
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE record(
+user_id BIGINT NOT NULL,
+command TEXT NOT NULL,
+count NOT NULL
+);
+COMMIT;
+EOF;
+$db->query($sql);
+?>
