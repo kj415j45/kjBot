27d6c9560e534ca371cdcd56f0e79122cdfd68d3
Shen LiangXiang <kj415j45@gmail.com>
2018-07-27 17:10:41 +0800

    Update build.php

--- build.php
+++ build.php
@@ -3,12 +3,11 @@ file_put_contents("composer-installer", file_get_contents("https://getcomposer.o
 exec("php composer-installer",$x,$rValue1);
 exec("php composer.phar install",$x,$rValue2);
 if(($rValue1!=$rValue2)||($rValue1!=0))
-    die();
+    die('安装 composer 失败');
 
 $here = __DIR__.'/';
 mkdir($here.'storage/data', 0777, true);
 mkdir($here.'storage/cache', 0777, true);
-fopen("storage/data/black.txt","a");
 $db = new SQLite3('storage/data/stat.db');
 
 $sql=file_get_contents($here.'stat.sql');
