182ecbd8685f9dc596690803a80e7cac032e787e
kj415j45 <kj415j45@gmail.com>
2018-07-30 07:57:34 +0900

    :bug: Fix issue

--- module/time/update.php
+++ module/time/update.php
@@ -7,7 +7,7 @@ use kjBot\SDK\CQCode;
 $web = file_get_contents($Text);
 if(false == $web)throw new \Exception('Update failed, no data found');
 
-preg_match_all('/https:\/\/img\.moegirl\.org\/common\/\S{1}\/\S{2}\/[a-zA-Z0-9_]*\.mp3/', $web, $result);
+preg_match_all('/https:\/\/img\.moegirl\.org\/common\/\S{1}\/\S{2}\/[%a-zA-Z0-9_]*\.mp3/', $web, $result);
 $start=count($result[0])-24-1;
 for($i=$start, $j=0; $i<$start+24 ; $i++, $j++){
     setData("time/{$j}.mp3", file_get_contents($result[0][$i]));
