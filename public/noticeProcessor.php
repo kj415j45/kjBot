e14d8af4660920555ba16e147a6f2aaad37da2db
kj415j45 <kj415j45@gmail.com>
2018-08-16 10:14:48 +0900

    :memo: Add TOS

--- public/noticeProcessor.php
+++ public/noticeProcessor.php
@@ -8,7 +8,7 @@ switch($Event['notice_type']){
         if($Event['user_id'] != config('bot')){
             $Queue[]= sendBack(CQCode::At($Event['user_id']).' 欢迎加入本群，请阅读群公告！');
         }else{
-            $Queue[]= sendBack('kjBot 已入驻本群，发送 '.config('prefix', '!').'help 查看帮助');
+            $Queue[]= sendBack('kjBot 已入驻本群，发送 '.config('prefix', '!').'help 查看帮助'."\nkjBot 用户协议：https://github.com/kjBot-Dev/TOS/blob/master/README.md");
         }
         break;
     case 'group_decrease':
